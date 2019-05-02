8fe24455e3dfb19b444d2513438b92410ea44200
deathbykender <45900750+abferris@users.noreply.github.com>
2019-05-02 10:25:27 -0700

    Fixing the scss

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import React from "react";
 import { connect } from "react-redux";
 
 import * as actions from "./store/actions.js";
-import "./styles/styles.scss"
 import app from "./app.module.scss";
 
 class App extends React.Component {
